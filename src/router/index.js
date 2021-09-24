e728abe06d11821d3324da346204e93d3a4c36dc
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-09-24 15:50:49 +0800

    chore: 分享

--- src/router/index.js
+++ src/router/index.js
@@ -93,6 +93,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, form, next) => {
+  alert(Env.getType().platformType);
   if (Env.getType().platformType === 'WX_GZ') {
     const url = window.location.href;
     const options = Util.getUrlOption(url);
