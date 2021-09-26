a3f9f95743c47bf64074bf2e9ff437ca9e224003
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-09-26 19:38:07 +0800

    chore: 分享

--- src/router/index.js
+++ src/router/index.js
@@ -107,8 +107,20 @@ router.beforeEach((to, form, next) => {
     const options = Util.getUrlOption(url);
     // localStorage.removeItem('userId');
     const userId = localStorage.getItem('userId');
-    if (userId === null && options.code === undefined) {
+    if (!userId) {
       alert(1);
+    }
+    if (userId === null) {
+      alert(2);
+    }
+    if (!options.code) {
+      alert(3);
+    }
+    if (options.code === undefined) {
+      alert(4);
+    }
+    if (!userId && !options.code) {
+      alert(5);
       const sourceId = options.channel || '';
       window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${
         Config.corpId
