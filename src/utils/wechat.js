b4492ca410d3b74275b4aa365097950b22ec3366
cj <cj@cjdeMacBook-Pro.local>
2021-09-28 17:22:49 +0800

    fix: wechat

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -12,10 +12,9 @@ const Wechat = {
     wxSignature = res.data;
   },
   setAgentConfig: (info, type) => {
-    console.log(wxSignature);
     wx.agentConfig({
-      corpid: sessionStorage.getItem('corpId'), // 必填，企业微信的corpid，必须与当前登录的企业一致
-      agentid: sessionStorage.getItem('agentid'), // 必填，企业微信的应用id （e.g. 1000247）
+      corpid: wxSignature.corpId, // 必填，企业微信的corpid，必须与当前登录的企业一致
+      agentid: sessionStorage.getItem('agentId'), // 必填，企业微信的应用id （e.g. 1000247）
       timestamp: wxSignature.timestamp, // 必填，生成签名的时间戳
       nonceStr: wxSignature.nonceStr, // 必填，生成签名的随机串
       signature: wxSignature.signature, // 必填，签名，见附录-JS-SDK使用权限签名算法
@@ -36,6 +35,7 @@ const Wechat = {
         // 回调
       },
       fail: (res) => {
+        alert(JSON.stringify(res));
         if (res.errMsg.indexOf('function not exist') > -1) {
           alert('版本过低请升级');
         }
