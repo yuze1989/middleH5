b0b302b026d1dfb5c4a2d00b096876496c0c5fc5
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-09-28 17:21:07 +0800

    chore: 分享

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -36,6 +36,7 @@ const Wechat = {
         // 回调
       },
       fail: (res) => {
+        alert(JSON.stringify(res));
         if (res.errMsg.indexOf('function not exist') > -1) {
           alert('版本过低请升级');
         }
