b7a16bc374cf2d3bdb8aa67ef7e5f7393a8d7a58
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-09-29 09:53:05 +0800

    chore: 路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -105,7 +105,7 @@ router.beforeEach((to, form, next) => {
   if (Env.getType().platformType === 'WX_GZ') {
     const url = window.location.href;
     const options = Util.getUrlOption(url);
-    // localStorage.removeItem('token');
+    localStorage.removeItem('token');
     const token = sessionStorage.getItem('token');
     if (!token && !options.code && options.appid) {
       const sourceId = options.channel || '';
@@ -116,6 +116,7 @@ router.beforeEach((to, form, next) => {
       }&response_type=code&scope=snsapi_userinfo&state=${sourceId}#wechat_redirect`;
       return;
     }
+    // alert(token);
     if (!token && options.code) {
       Http.post('/scrm/wechat/get-oauth-user-info', {
         corpId: options.appid,
