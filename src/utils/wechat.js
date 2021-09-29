8f3302995248cb0638befd4d507e261912585b44
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-09-29 20:10:07 +0800

    chore: 分享

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -5,8 +5,6 @@ let wxSignature;
 
 const Wechat = {
   setWxConfig: async () => {
-    const corpid = sessionStorage.getItem('corpId');
-    alert(corpid);
     const res = await Http.post('/scrm/wechat/js-api-signature', {
       corpId: corpid,
       url: window.location.href.split('#')[0],
