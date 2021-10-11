8c628f80dff382c7383b68d7c0f2cf27fcfdb8ca
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-11 17:17:08 +0800

    chore: 素材权限

--- src/utils/http.js
+++ src/utils/http.js
@@ -11,6 +11,11 @@ const instance = axios.create({
 });
 // 请求拦截添加头部参数等
 instance.interceptors.request.use((config) => {
+  if (!config) {
+    store.dispatch('SETERRSTATE', 1);
+  } else {
+    store.dispatch('SETERRSTATE', 0);
+  }
   const globalOptStr = sessionStorage.getItem('globalOpt');
   const globalOpt = !globalOptStr ? {} : JSON.parse(globalOptStr);
   const configTemp = config;
@@ -20,17 +25,13 @@ instance.interceptors.request.use((config) => {
   configTemp.headers.token = token;// || 'mockToken';
   return config;
 }, (error) => {
+  store.dispatch('SETERRSTATE', 1);
   Promise.reject(error);
 });
 
 // 响应拦截
 instance.interceptors.response.use(
   (response) => {
-    if (!response) {
-      store.dispatch('SETERRSTATE', 1);
-    } else {
-      store.dispatch('SETERRSTATE', 0);
-    }
     const { data } = response;
     // Util.go(data.errCode);
     // if (errorList[data.errCode]) {
@@ -41,7 +42,6 @@ instance.interceptors.response.use(
   },
   // (response) => (response.data),
   (error) => {
-    store.dispatch('SETERRSTATE', 1);
     // if (errorList.errCode) {
     //   window.location.href = `${Config.redirect_uri}/middleH5/jurisdiction?msg=
     //   ${errorList.errCode[0]}&type=${errorList.errCode[1]}`;
