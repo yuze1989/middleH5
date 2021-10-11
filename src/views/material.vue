bab20eaef9ccd4812c3a9ea906e45b3d79c46de6
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-11 16:28:00 +0800

    chore: 素材权限

--- src/views/material.vue
+++ src/views/material.vue
@@ -32,7 +32,7 @@
         </List>
       </div>
     </PullRefresh>
-    <jurisdiction :err="err" :type="errType" v-else></jurisdiction>
+    <jurisdiction :err="err" :type="errType" v-if="!whether"></jurisdiction>
   </div>
 </template>
 
