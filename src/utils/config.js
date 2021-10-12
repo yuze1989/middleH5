2db143ad5cac7ccc42a504c53352b25d8115aad6
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-12 14:57:38 +0800

    chore: 增加调试代码

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 import Env from './deviceinfo';
 
-const redirectUri = process.env.NODE_ENV === 'production' ? 'https://mobile.juzhunshuyu.com' : 'https://test-scrm.juzhunshuyu.com';
+const redirectUri = process.env.NODE_ENV === 'production' ? 'https://test-scrm.juzhunshuyu.com' : 'https://test-scrm.juzhunshuyu.com';
 
 export default {
   redirect_uri: redirectUri,
