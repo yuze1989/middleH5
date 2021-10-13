73a3b0644066f321ca658bfe604dce2878cbac5c
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-13 15:35:29 +0800

    chore: 修改缓存方式

--- src/router/index.js
+++ src/router/index.js
@@ -110,7 +110,7 @@ router.beforeEach((to, form, next) => {
     const corpId = localStorage.getItem('corpId');
     const src = window.location.pathname;
     // if (!token && !options.code && options.appid) {
-    if (options.appid === corpId) {
+    if (options.appid !== corpId) {
       const sourceId = options.channel || '';
       window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${
         options.appid
