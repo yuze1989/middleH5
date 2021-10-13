f5e20a1f66754f43f34c7b1a6744f6851edb0736
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-13 20:08:29 +0800

    chore: 修改缓存方式

--- src/router/index.js
+++ src/router/index.js
@@ -105,9 +105,14 @@ router.beforeEach((to, form, next) => {
     const url = window.location.href;
     // localStorage.removeItem('token');
     const options = Util.getUrlOption(url);
+    // localStorage.clear();
     const token = localStorage.getItem('token');
     const corpId = localStorage.getItem('corpId');
     const src = window.location.pathname;
+    if (options.appid !== corpId && token && !options.code) {
+      localStorage.clear();
+      next();
+    }
     // if (!token && !options.code && options.appid) {
     if (!token && options.appid && options.appid !== corpId && !options.code) {
       const sourceId = options.channel || '';
@@ -117,9 +122,6 @@ router.beforeEach((to, form, next) => {
         encodeURIComponent(`${Config.redirect_uri}${src}?channel=${sourceId}&appid=${options.appid}&batchNo=${options.batchNo || ''}`)
       }&response_type=code&scope=snsapi_userinfo&state=${sourceId}#wechat_redirect`;
       return;
-    }
-    if (options.appid !== corpId && token && !options.code) {
-      localStorage.clear();
     }
     if (!token && options.code) {
       Http.post('/scrm/wechat/get-oauth-user-info', {
@@ -154,7 +156,6 @@ router.beforeEach((to, form, next) => {
       });
       return;
     }
-
     // return;
   }
   next();
