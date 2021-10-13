38ebe34678737edf43da766963c2775474184607
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-13 15:51:24 +0800

    chore: 修改缓存方式

---
+++ src/views/portraitDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box">
+
+  </div>
+</template>
+<script>
+// import Http from '../utils/http';
+// import Utils from '../utils/util';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+
+  },
+};
+</script>
+<style scoped>
+  .box{
+
+  }
+</style>
