c8ec802ff0b817e98812610ced03ff920004b9c4
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-15 10:42:13 +0800

    chore: 处理token过期

--- src/router/index.js
+++ src/router/index.js
@@ -106,6 +106,7 @@ router.beforeEach(async (to, form, next) => {
     const options = Util.getUrlOption(url);
     const corpId = localStorage.getItem('corpId');
     const src = window.location.pathname;
+    console.log(options.appid, corpId, '///////////////////');
     if (options.appid !== corpId && options.appid) {
       localStorage.clear();
     }
