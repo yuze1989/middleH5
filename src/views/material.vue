ec8cb2f28359727f8cc21a86d8de87654e1f1af1
cj <cj@cjdeMacBook-Pro.local>
2021-10-19 13:46:23 +0800

    chore: middleH5 -> sh5

--- src/views/material.vue
+++ src/views/material.vue
@@ -240,7 +240,7 @@ export default {
           if (res.success) {
             data.news = {
               // H5消息页面url 必填
-              link: url || `${Config.redirect_uri}/middleH5/details?id=${res.data}`,
+              link: url || `${Config.redirect_uri}/sh5/details?id=${res.data}`,
               title: obj.title, // H5消息标题
               desc: obj.description, // H5消息摘要
               imgUrl: obj.coverPicUrl, // H5消息封面图片URL
