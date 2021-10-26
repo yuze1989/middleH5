1ce29b34ec507e1a084a5286ea65f137507375d6
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-10-26 17:40:34 +0800

    chore: 客户sop分享

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -13,6 +13,29 @@ const Wechat = {
   },
   setAgentConfig: async (info, type, func) => {
     await Wechat.setWxConfig();
+    wx.config({
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: wxSignature.corpId, // 必填，公众号的唯一标识
+      timestamp: wxSignature.timestamp, // 必填，生成签名的时间戳
+      nonceStr: wxSignature.nonceStr, // 必填，生成签名的随机串
+      signature: wxSignature.signature, // 必填，签名，见附录1
+      jsApiList: ['updateAppMessageShareData', 'openExistedChatWithMsg', 'updateTimelineShareData'], // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      openTagList: ['wx-open-launch-weapp'],
+    });
+    wx.error((res) => {
+      console.log(res, '/////////');
+    });
+    wx.checkJsApi({
+      jsApiList: ['updateAppMessageShareData', 'openExistedChatWithMsg', 'updateTimelineShareData'],
+      openTagList: ['wx-open-launch-weapp'],
+      success: (res) => {
+        // window.isSetWxconfig = true // 在window中添加isSetWxconfig,判断是否进行页面刷新
+        console.log('检查jsapi==', res);
+        // alert(JSON.stringify(res));
+        // 以键值对的形式返回，可用的api值true，不可用为false
+        // 如：{'checkResult':{'chooseImage':true},'errMsg':'checkJsApi:ok'}
+      },
+    });
     wx.agentConfig({
       corpid: wxSignature.corpId, // 必填，企业微信的corpid，必须与当前登录的企业一致
       agentid: localStorage.getItem('agentId'), // 必填，企业微信的应用id （e.g. 1000247）
