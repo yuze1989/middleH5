43a3c6766c52ec08a352735bccaa5b7ebd3aadce
2556573535 <a2556573535@2556573535deMacBook-Pro.local>
2021-11-15 15:58:33 +0800

    fix：下拉刷新样式调整

--- src/views/portraitDetails.vue
+++ src/views/portraitDetails.vue
@@ -255,7 +255,7 @@ export default {
           platformCode: 'ALL',
           pageIndex: that.pageIndex,
           pageSize: 20,
-          receiverMobile: 18939499727, // that.useData.mobile,
+          receiverMobile: that.useData.mobile, // that.useData.mobile,
         };
       }
       Http.post(url, data, '').then((res) => {
