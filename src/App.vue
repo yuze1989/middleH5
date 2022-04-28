98139f5b51c58a62bacca0ae0a81048540b85540
刘敏 <mac@liumin.local>
2022-04-28 17:33:05 +0800

    feat: 修改图片

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
   name: 'app',
   data() {
     return {
-      includeArr: ['workbench', 'customer'],
+      includeArr: ['workbench', 'customer', 'my'],
     };
   },
   methods: {
